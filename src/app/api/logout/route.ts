d9e1027029af1e74460c134c526bbaed6073ac6c
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-31 22:26:03 +0700

    update

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 
-export default async function GET() {
+export async function GET() {
     try {
         const cookieStore = await cookies()
 
